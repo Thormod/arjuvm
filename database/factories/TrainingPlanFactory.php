a6cdb03d9f5263bd1ff7d68549c28cfdd7d4689c
Thormod <thjodhild@gmail.com>
2018-07-22 12:05:08 -0500

    Training plan details update

--- database/factories/TrainingPlanFactory.php
+++ database/factories/TrainingPlanFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\TrainingPlan::class, function (Faker $faker) {
         'description' => $faker->paragraph($nbSentences = 30, $variableNbSentences = true),
         'price' => rand(50,200),
         'discount' => rand(10,50),
-        'video_url' => $faker->randomElement(['https://www.youtube.com/watch?v=SIeBuGBEQ9w', 'https://www.youtube.com/watch?v=CGkjggbXLNs', 'https://www.youtube.com/watch?v=iecyRM7zBGs&t=589s']),
+        'video_url' => $faker->randomElement(['https://www.youtube.com/embed/SIeBuGBEQ9w?rel=0', 'https://www.youtube.com/embed/CGkjggbXLNs?rel=0', 'https://www.youtube.com/embed/iecyRM7zBGs?rel=0']),
         'image_url' => $faker->imageUrl($width = 555, $height = 335)
     ];
 });
