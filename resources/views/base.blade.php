a91bccf74b9c0618ddf6c003712c2f4d067e78cf
Thormod <thjodhild@gmail.com>
2018-08-16 11:06:24 -0500

    Adding jquery

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -45,7 +45,8 @@
         </a>
         @include('layouts.footer')
         <!-- Include JS -->
-        <script src="/js/vendor/jquery-3.2.0.min.js"></script>
+        <script src="/js/jquery-3.2.0.min.js"></script>
+        <script src="/js/modernizr-custom.js"></script>
         <script src="/js/bootstrap.min.js"></script>
         <script src="/js/slick.min.js"></script>
         <script src="/js/plugins.js"></script>
